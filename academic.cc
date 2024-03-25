82190e99db62109754520991b13cc3ab2796462c
atshah <atshah@uwaterloo.ca>
2024-03-25 19:21:02 -0400

    academic done

--- academic.cc
+++ academic.cc
@@ -11,14 +11,15 @@ Academic::Academic(string monopoly_block, string name, int purchase_cost,
              upgrade_level{upgrade_level}, rentMoney{rentMoney}, bought{false}, mortgaged{false} {}
 
 
-string Academic::getBlock() {
-    return monopoly_block;
-}
+// getters
+string Academic::getBlock() {return monopoly_block;}
+string Academic::getName() {return name;}
+int Academic::getCost() {return purchase_cost;}
+int Academic::getUpgrade_cost() {return upgrade_cost;}
+int Academic::getUpgradeLevel() {return upgrade_level;}
+bool Academic::isBought() {return bought;}
+bool Academic::isMortgaged() {return mortgaged;}
 
-
-string Academic::getName() {
-    return name;
-}
 
 void Academic::upgrade(Player *p) {
     p->changeCash(upgrade_cost, false);
@@ -30,7 +31,8 @@ void Academic::mortgage(Player *p) {
     mortgaged = true;
 }
 
-void Academic::buy(Player * p) {
+void Academic::buy(Player *p) {
     p->changeCash(purchase_cost, false); 
+    p->addProperty(this);
     bought = true;
 }
