e6eb82140139660b1963c1bb010649cfe98a4457
frmadara <frmadara@uwaterloo.ca>
2024-03-25 20:21:30 -0400

    finished jail square

---
+++ jail.h
@@ -0,0 +1,16 @@
+#ifndef __JAIL_H_
+#define __JAIL_H_
+#include <map>
+#include "square.h"
+#include "player.h"
+
+class Jail : public Square {
+    std::map<Player*, int> jailedTurns;
+
+    public: 
+        Jail();
+        void sendToJail(Player* player);
+        bool handleMove(Player* player);
+};
+
+#endif
