d9519cae50021ab4fff603a01ffdbd1eff0eadea
atshah <atshah@uwaterloo.ca>
2024-04-04 11:55:17 -0400

    .h file done

--- board_graphics.h
+++ board_graphics.h
@@ -4,13 +4,20 @@
 #include <iostream>
 #include <string>
 #include "window.h"
+#include "player.h"
+#include <vector>
 
 class boardGraphics {
-    int height;
-    int width;
-    int tile_height;
-    int tile_width;
+    int height = 500;
+    int width = 500;
+    int tile_height = 45;
+    int tile_width = 45;
     Xwindow w;
+
+    public:
+    boardGraphics();
+    void printImprovements(int tile_index);
+    void printPlayers(int tile_index, std::vector<Player *> & p);
 };
 
 #endif
